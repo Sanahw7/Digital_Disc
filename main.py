06c0095da63fefa873269c01f4dac888954fb4ba
Sanah White <sanahwhite7@gmail.com>
2025-12-12 10:49:46 -0500

    login/register

--- main.py
+++ main.py
@@ -53,3 +53,13 @@ def product_page(product_id):
     connection.close()
 
     return render_template("product.html.jinja", product=result)
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html.jinja")
+    
+
+@app.route("/register")
+def register():
+    return render_template("register.html.jinja")
